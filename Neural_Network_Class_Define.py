1652b137a9118106eb39bd2c93b6102d3f903a60
Zhang ZMWJ <zwj415251@163.com>
2023-09-30 09:34:01 +0800

    Initial Setting

---
+++ Neural_Network_Class_Define.py
@@ -0,0 +1,225 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Neural_Network_Classfor the training of geotechnical data
+# By Weijie Zhang, Hohai University
+# ----------------------------------------------------------------------------------------------------------------------
+import torch
+import torch.nn as nn
+import torch.optim
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+import torchmetrics as tm
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Input --> Linear 1 --> Activation function 1 --> Linear 2 --> Activation function 2 --> Linear 3 --> Output
+# ----------------------------------------------------------------------------------------------------------------------
+class Neural_Network_Class(nn.Module):  # define the three-layer neural network
+    # define the layer number of neural network
+    def __init__(self, input_size, hidden_size, output_size, actifun_type1, actifun_type2):
+        super(Neural_Network_Class, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        if actifun_type1 == 1:
+            self.actfun1 = nn.Sigmoid()
+        elif actifun_type1 == 2:
+            self.actfun1 = nn.ReLU()
+        elif actifun_type1 == 3:
+            self.actfun1 = nn.Tanh()
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        if actifun_type2 == 1:
+            self.actfun2 = nn.Sigmoid()
+        elif actifun_type2 == 2:
+            self.actfun2 = nn.ReLU()
+        elif actifun_type2 == 3:
+            self.actfun2 = nn.Tanh()
+        self.predict = nn.Linear(hidden_size, output_size)
+
+    # define the forward propagation process
+    def forward(self, input):
+        out = self.linear1(input)
+        out = self.actfun1(out)
+        out = self.linear2(out)
+        out = self.actfun2(out)
+        out = self.predict(out)
+        return out
+
+    # define the training process
+    def training_process(self, learn_r, bat_size, train_loop, optim_type, loss_type, data_dir, slice_num):
+        # load data and label from file
+        data_numpy = np.loadtxt(data_dir, dtype=np.float32, delimiter=',', skiprows=0)
+        data_tensor = torch.from_numpy(data_numpy)
+        features_o = data_tensor[:, :slice_num]
+        labels_o = data_tensor[:, slice_num]
+        # normalization
+        feat_max, _ = torch.max(features_o, dim=0)
+        feat_min, _ = torch.min(features_o, dim=0)
+        label_max, _ = torch.max(labels_o, dim=0)
+        label_min, _ = torch.min(labels_o, dim=0)
+        features = (features_o - feat_min) / (feat_max - feat_min)
+        labels = (labels_o - label_min) / (label_max - label_min)
+        train_dataset = torch.utils.data.TensorDataset(features, labels)
+
+        # define the optimizer
+        if optim_type == 1:  # using the Stochastic Gradient Descent method
+            optimizer = torch.optim.SGD(self.parameters(), learn_r)
+        elif optim_type == 2:  # using the Adam method
+            optimizer = torch.optim.Adam(self.parameters(), learn_r)
+        elif optim_type == 3:  # using the RMSprop method
+            optimizer = torch.optim.RMSprop(self.parameters(), learn_r)
+        elif optim_type == 4:  # using the Adagrad method
+            optimizer = torch.optim.Adagrad(self.parameters(), learn_r)
+        print(optimizer)
+        # define the loss function
+        if loss_type == 1:
+            criterion = nn.MSELoss()  # mean squared error function
+        elif loss_type == 2:
+            criterion = nn.CrossEntropyLoss()  # cross entropy error function
+        elif loss_type == 3:
+            criterion = nn.PoissonNLLLoss()  # PoissonNLLLoss function
+        print(criterion)
+        # define the array for the loss function
+        loss_holder = []
+        # set the loss as infinity: loss_value < pre_loss_value, save model
+        loss_value = np.inf
+        step = 0
+        # loading the training data
+        for t_id in range(train_loop):
+            train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=bat_size, shuffle=True)
+            for batch_idx, (data, label) in enumerate(train_loader):
+                # the result of forward process
+                output = torch.squeeze(self.forward(data))
+                # loss value
+                loss = criterion(output, label)
+                # back propagation: all the gradients are zero before back propagation.
+                optimizer.zero_grad()
+                loss.backward()
+                # update parameters
+                optimizer.step()
+                # log the loss value
+                if batch_idx % bat_size == 0:
+                    step += 1
+                    loss_holder.append([step, loss.detach().numpy() / bat_size])
+
+                if batch_idx % bat_size == 0 and loss < loss_value:
+                    torch.save(self.state_dict(), '0-model.pt')
+                    loss_value = loss
+
+        return loss_holder
+
+    # define the training and testing process
+    def training_testing_process(self, learn_r, bat_size, train_loop, optim_type, loss_type, train_num, test_num,
+                                 train_data_dir, test_data_dir, slice_num):
+        # load data and label from training file and testing file
+        train_data_numpy = np.loadtxt(train_data_dir, dtype=np.float32, delimiter=',', skiprows=0)
+        train_data_tensor = torch.from_numpy(train_data_numpy)
+        train_features_o = train_data_tensor[:, :slice_num]
+        train_labels_o = train_data_tensor[:, slice_num]
+        # normalization
+        feat_max, _ = torch.max(train_features_o, dim=0)
+        feat_min, _ = torch.min(train_features_o, dim=0)
+        label_max, _ = torch.max(train_labels_o, dim=0)
+        label_min, _ = torch.min(train_labels_o, dim=0)
+        train_features = (train_features_o - feat_min) / (feat_max - feat_min)
+        train_labels = (train_labels_o - label_min) / (label_max - label_min)
+        train_dataset = torch.utils.data.TensorDataset(train_features, train_labels)
+
+        test_data_numpy = np.loadtxt(test_data_dir, dtype=np.float32, delimiter=',', skiprows=0)
+        test_data_tensor = torch.from_numpy(test_data_numpy)
+        test_features = test_data_tensor[:, :slice_num]
+        test_labels = test_data_tensor[:, slice_num]
+        test_features = (test_features - feat_min) / (feat_max - feat_min)
+        test_labels = (test_labels - label_min) / (label_max - label_min)
+        test_dataset = torch.utils.data.TensorDataset(test_features, test_labels)
+
+        # define the optimizer
+        if optim_type == 1:  # using the Stochastic Gradient Descent method
+            optimizer = torch.optim.SGD(self.parameters(), learn_r)
+        elif optim_type == 2:  # using the Adam method
+            optimizer = torch.optim.Adam(self.parameters(), learn_r)
+        elif optim_type == 3:  # using the RMSprop method
+            optimizer = torch.optim.RMSprop(self.parameters(), learn_r)
+        elif optim_type == 4:  # using the Adagrad method
+            optimizer = torch.optim.Adagrad(self.parameters(), learn_r)
+        print(optimizer)
+        # define the loss function
+        if loss_type == 1:
+            criterion = nn.MSELoss()  # mean squared error function
+        elif loss_type == 2:
+            criterion = nn.CrossEntropyLoss()  # cross entropy error function
+        elif loss_type == 3:
+            criterion = nn.PoissonNLLLoss()   # PoissonNLLLoss function
+        print(criterion)
+        # error function
+        Accuracy_Fun_1 = tm.PearsonCorrCoef()
+        Accuracy_Fun_2 = tm.CosineSimilarity()
+        # define the array for the loss function and similarity score
+        loss_holder = []
+        simu_score_train = []
+        simu_score_test = []
+        # set the loss as infinity: loss_value < pre_loss_value, save model
+        loss_value = np.inf
+        step = 0
+        # loading the training data
+        for t_id in range(train_loop):
+            train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=bat_size, shuffle=True)
+            for batch_idx_1, (data_1, label_1) in enumerate(train_loader):
+                # the result of forward process
+                output_1 = torch.squeeze(self.forward(data_1))
+                # loss value
+                loss = criterion(output_1, label_1)
+                # back propagation: all the gradients are zero before back propagation.
+                optimizer.zero_grad()
+                loss.backward()
+                # update parameters
+                optimizer.step()
+                # log the loss value
+                if batch_idx_1 % bat_size == 0:
+                    step += 1
+                    loss_holder.append([step, loss.detach().numpy() / bat_size])
+
+                if batch_idx_1 % bat_size == 0 and loss < loss_value:
+                    torch.save(self.state_dict(), '0-model.pt')
+                    loss_value = loss
+
+            # calculating the similarity score for the training data
+            train_loader_all = torch.utils.data.DataLoader(train_dataset, batch_size=train_num, shuffle=False)
+            for batch_idx_2, (data_2, label_2) in enumerate(train_loader_all):
+                # the result of forward process
+                output_2 = torch.squeeze(self.forward(data_2))
+                score_train_per = Accuracy_Fun_1(label_2, output_2)
+                score_train_cos = Accuracy_Fun_2(label_2, output_2)
+                simu_score_train.append([t_id, score_train_per.detach().numpy(), score_train_cos.detach().numpy()])
+
+            # calculating the similarity score for the testing data
+            test_loader_all = torch.utils.data.DataLoader(test_dataset, batch_size=test_num, shuffle=False)
+            for batch_idx_3, (data_3, label_3) in enumerate(test_loader_all):
+                # the result of forward process
+                output_3 = torch.squeeze(self.forward(data_3))
+                score_test_per = Accuracy_Fun_1(label_3, output_3)
+                score_test_cos = Accuracy_Fun_2(label_3, output_3)
+                simu_score_test.append([t_id, score_test_per.detach().numpy(), score_test_cos.detach().numpy()])
+
+        return loss_holder, simu_score_train, simu_score_test, feat_max, feat_min, label_max, label_min
+
+    # define the process of loading model state and predict new data
+    def model_load_predict(self, pred_data_dir, pred_result_dir):
+        # loading the model parameters
+        self.load_state_dict(torch.load('0-model.pt'))
+        feat_max = torch.load('2-feat-max.pth')
+        feat_min = torch.load('2-feat-min.pth')
+        label_max = torch.load('2-label-max.pth')
+        label_min = torch.load('2-label-min.pth')
+        # load data from predicting file
+        pred_data_numpy = np.loadtxt(pred_data_dir, dtype=np.float32, delimiter=',', skiprows=0)
+        pred_data_tensor_o = torch.from_numpy(pred_data_numpy)
+        pred_data_tensor = (pred_data_tensor_o - feat_min) / (feat_max - feat_min)
+        # forward process
+        if pred_data_tensor.ndim == 1:
+            pred_result = self.forward(torch.unsqueeze(pred_data_tensor, dim=1))
+            pred_result = pred_result * (label_max - label_min) + label_min
+            pred_tensor = torch.cat([torch.unsqueeze(pred_data_tensor_o, dim=1), pred_result], 1)
+        else:
+            pred_result = self.forward(pred_data_tensor)
+            pred_result = pred_result * (label_max - label_min) + label_min
+            pred_tensor = torch.cat([pred_data_tensor_o, pred_result], 1)
+        np.savetxt(pred_result_dir, pred_tensor.detach().numpy(), fmt='%.6f', delimiter=',')
---
+++ main.py
@@ -0,0 +1,155 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Neural_Network_Classfor the training of geotechnical data
+# By Weijie Zhang, Hohai University
+# ----------------------------------------------------------------------------------------------------------------------
+import os
+import torch
+from torch.nn import init
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.font_manager as plt_fm
+from Neural_Network_Class_Define import Neural_Network_Class
+
+# ----------------------------------------------------------------------------------------------------------------------
+# main program
+# define the running type： 1--train; 2--train and test; 3--load model state and predict new data
+run_type = 2
+# ----------------------------------------------------------------------------------------------------------------------
+# define the file path
+# training data file path
+if os.sep == "/":  # linux platform
+    train_data_dir = r'./train-data/train-data.csv'
+else:  # windows platform
+    train_data_dir = r'.\\train-data\\train-data.csv'
+# testing data file path
+if os.sep == "/":  # linux platform
+    test_data_dir = r'./test-data/test-data.csv'
+else:  # windows platform
+    test_data_dir = r'.\\test-data\\test-data.csv'
+# predicting data file path
+if os.sep == "/":  # linux platform
+    pred_data_dir = r'./predict/data.csv'
+else:  # windows platform
+    pred_data_dir = r'.\\predict\\data.csv'
+# predicting result file path
+if os.sep == "/":  # linux platform
+    pred_result_dir = r'./predict/result.csv'
+else:  # windows platform
+    pred_result_dir = r'.\\predict\\result.csv'
+# ----------------------------------------------------------------------------------------------------------------------
+# define the number of data in training file and testing file
+train_data_num = 25
+test_data_num = 5
+slice_num = 2  # define the number of features
+# ----------------------------------------------------------------------------------------------------------------------
+# Input --> Linear 1 --> Activation function 1 --> Linear 2 --> Activation function 2 --> Linear 3 --> Output
+#    (input_size, hidden_size)            (hidden_size, hidden_size)             (hidden_size, output_size)
+# ----------------------------------------------------------------------------------------------------------------------
+# define the sizes of input vector, hidden vector and output vector
+input_size = slice_num
+hidden_size = 10
+output_size = 1
+# ----------------------------------------------------------------------------------------------------------------------
+# Input --> Linear 1 --> Activation function 1 --> Linear 2 --> Activation function 2 --> Linear 3 --> Output
+# the type of activation function: 1--sigmoid function; 2--ReLU function; 3--tanh function
+actifun_type1 = 2
+actifun_type2 = 1
+# ----------------------------------------------------------------------------------------------------------------------
+# the type of optimizer: 1--SGD; 2--Adam; 3--RMSprop; 4--Adagrad
+optim_type = 3
+# the type of loss function: 1--mean squared error function; 2--cross entropy error function; 3--PoissonNLLLoss function
+loss_type = 1
+# ----------------------------------------------------------------------------------------------------------------------
+# define the batch size
+batch_size = 4
+# define the learning rate
+learn_r = 0.001
+# define the training cycle
+train_loop = 1000
+# ----------------------------------------------------------------------------------------------------------------------
+# setting font
+font_tnr_reg = plt_fm.FontProperties('Times New Roman', size=14, stretch=0)
+# ----------------------------------------------------------------------------------------------------------------------
+# set the model instance
+Net_Model = Neural_Network_Class(input_size, hidden_size, output_size, actifun_type1, actifun_type2)
+print(Net_Model)
+# ----------------------------------------------------------------------------------------------------------------------
+# define training function of Net_Model
+# train the model with training data
+if run_type == 1:
+    # initializing the weight parameters
+    for layer in Net_Model.modules():
+        if isinstance(layer, nn.Linear):
+            init.xavier_uniform_(layer.weight)
+
+    loss_holder = Net_Model.training_process(learn_r, batch_size, train_loop, optim_type, loss_type, train_data_dir,
+                                             slice_num)
+    # plot the relationship between loss_value and iteration step
+    fig = plt.figure()
+    loss_df = pd.DataFrame(loss_holder, columns=['step', 'loss'])
+    plt.plot(loss_df['loss'].values, 'ro', markersize=2)
+    plt.xticks(fontproperties=font_tnr_reg)
+    plt.yticks(fontproperties=font_tnr_reg)
+    plt.xlabel('Iteration step', fontproperties=font_tnr_reg)
+    plt.ylabel('Loss', fontproperties=font_tnr_reg)
+    plt.show()
+# ----------------------------------------------------------------------------------------------------------------------
+# define training and testing function of Net_Model
+# testing the model with test data
+if run_type == 2:
+    # initializing the weight parameters
+    for layer in Net_Model.modules():
+        if isinstance(layer, nn.Linear):
+            init.xavier_uniform_(layer.weight)
+
+    loss_holder, simu_score_train, simu_score_test, feat_max, feat_min, label_max, label_min = \
+        Net_Model.training_testing_process(learn_r, batch_size, train_loop,
+                                           optim_type, loss_type,
+                                           train_data_num, test_data_num,
+                                           train_data_dir, test_data_dir,
+                                           slice_num)
+    # plot the relationship between loss_value and iteration step
+    fig = plt.figure()
+    loss_df = pd.DataFrame(loss_holder, columns=['step', 'loss'])
+    plt.plot(loss_df['loss'].values, 'ro', markersize=2)
+    plt.xticks(fontproperties=font_tnr_reg)
+    plt.yticks(fontproperties=font_tnr_reg)
+    plt.xlabel('Iteration step', fontproperties=font_tnr_reg)
+    plt.ylabel('Loss', fontproperties=font_tnr_reg)
+    plt.show()
+    # plot the relationship between loss_value and iteration step
+    score_train_df = pd.DataFrame(simu_score_train, columns=['step', 'score-per', 'score-cos'])
+    score_test_df = pd.DataFrame(simu_score_test, columns=['step', 'score-per', 'score-cos'])
+
+    fig2 = plt.figure()
+    plt.plot(score_train_df['score-per'].values, 'g*', markersize=2)
+    plt.plot(score_test_df['score-per'].values, 'm*', markersize=2)
+    plt.xticks(fontproperties=font_tnr_reg)
+    plt.yticks(fontproperties=font_tnr_reg)
+    plt.xlabel('Iteration step', fontproperties=font_tnr_reg)
+    plt.ylabel('Pearson Coefficient', fontproperties=font_tnr_reg)
+    plt.show()
+
+    fig3 = plt.figure()
+    plt.plot(score_train_df['score-cos'].values, 'g*', markersize=2)
+    plt.plot(score_test_df['score-cos'].values, 'm*', markersize=2)
+    plt.xticks(fontproperties=font_tnr_reg)
+    plt.yticks(fontproperties=font_tnr_reg)
+    plt.xlabel('Iteration step', fontproperties=font_tnr_reg)
+    plt.ylabel('Cosine similarity', fontproperties=font_tnr_reg)
+    plt.show()
+
+    # save training data in the log file
+    loss_df.to_csv('1-loss-process.txt', sep='\t', index=False)
+    score_train_df.to_csv('1-score-train-process.txt', sep='\t', index=False)
+    score_test_df.to_csv('1-score-test-process.txt', sep='\t', index=False)
+    torch.save(feat_max, '2-feat-max.pth')
+    torch.save(feat_min, '2-feat-min.pth')
+    torch.save(label_max, '2-label-max.pth')
+    torch.save(label_min, '2-label-min.pth')
+# ----------------------------------------------------------------------------------------------------------------------
+# define state loading and prediction function of Net_Model
+if run_type == 3:
+    Net_Model.model_load_predict(pred_data_dir, pred_result_dir)
+# ----------------------------------------------------------------------------------------------------------------------
